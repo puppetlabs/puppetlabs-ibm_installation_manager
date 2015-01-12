8a99f4c80dea06105179a9d56e8443a32107673c
Josh Beard <beard@puppetlabs.com>
2015-01-12 16:01:09 -0500

    Chomp the timestamp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class ibm_installation_manager::params {
-  $timestamp  = generate('/bin/date', '+%Y%d%m_%H:%M:%S')
+  $timestamp  = chomp(generate('/bin/date', '+%Y%d%m_%H:%M:%S'))
   $source_dir = '/opt/IBM/tmp'
   $base_dir   = '/opt/IBM'
   $options    = "–acceptLicense -sP –log /tmp/IM_install.${timestamp}.log.xml"
