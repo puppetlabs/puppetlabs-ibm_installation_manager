27c667c91235329aa9f7875e0435c57ac7be8a85
Josh Beard <beard@puppetlabs.com>
2015-01-12 16:06:43 -0500

    Correct hypen encoding
    The fucking hyphen had bullshit encoding.  This commit fixes that.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
 class ibm_installation_manager::params {
-  $timestamp  = chomp(generate('/bin/date', '+%Y%d%m_%H:%M:%S'))
+  $timestamp  = chomp(generate('/bin/date', '+%Y%d%m_%H%M%S'))
   $source_dir = '/opt/IBM/tmp'
   $base_dir   = '/opt/IBM'
-  $options    = "–acceptLicense -sP –log /tmp/IM_install.${timestamp}.log.xml"
+  $options    = "-acceptLicense -sP -log /tmp/IM_install.${timestamp}.log.xml"
   $user       = 'root'
   $group      = 'root'
 }
