3215670695133fc1430f99107ff08553f4085d20
Josh Beard <beard@puppetlabs.com>
2015-01-26 13:34:47 -0700

    Add example usage of ibm_impkg

---
+++ examples/ibm_impkg.pp
@@ -0,0 +1,8 @@
+# com.ibm.websphere.NDTRIAL.v85_8.5.5000.20130514_1044
+ibm_impkg { 'com.ibm.websphere.NDTRIAL.v85':
+  ensure     => 'present',
+  package    => 'com.ibm.websphere.NDTRIAL.v85',
+  version    => '8.5.5000.20130514_1044',
+  target     => '/opt/IBM/beard/WebSphere85/AppServer',
+  repository => '/vagrant/ibm/was/repository.config',
+}
