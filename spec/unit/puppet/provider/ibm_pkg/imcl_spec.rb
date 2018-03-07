39a47120f669fc8a7ce766fda7c30c04c4fb850d
Tony Thayer <tony.thayer@pgn.com>
2018-03-07 11:56:23 -0800

    Refactored manifest so non-admin works Changed variables to use puppet4+ data types Updated unit test to check for bad directory name when non-admin user passed. Changed spec tests to pass integers instead of string values for timeouts.

--- spec/unit/puppet/provider/ibm_pkg/imcl_spec.rb
+++ spec/unit/puppet/provider/ibm_pkg/imcl_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Puppet::Type.type(:ibm_pkg).provider(:imcl) do
   describe '#installed_file' do
-    context 'non-root user' do
+    context 'non-root user with user and user_home provided' do
       let(:resource) do
         Puppet::Type::Ibm_pkg.new(
           name: 'foo',
@@ -14,6 +14,19 @@ describe Puppet::Type.type(:ibm_pkg).provider(:imcl) do
 
       it 'returns the non-root path' do
         expect(provider.installed_file).to eq('/home/webadmin/var/ibm/InstallationManager/installed.xml')
+      end
+    end
+    context 'non-root user with only user parameter provided' do
+      let(:resource) do
+        Puppet::Type::Ibm_pkg.new(
+          name: 'foo',
+          user: 'webadmin',
+        )
+      end
+      let(:provider) { Puppet::Type.type(:ibm_pkg).provider(:imcl).new(resource) }
+
+      it 'should provide the wrong directory if user_home is not passed.' do
+        expect(provider.installed_file).not_to eq('/home/webadmin/var/ibm/InstallationManager/installed.xml')
       end
     end
     context 'root user' do
